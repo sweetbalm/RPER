c171e916e8dae5938ccc2b3f38e1ec0fb3d9c2a4
sweetbalm <r.ac.hel.ghha@gmail.com>
2026-03-25 15:50:21 +0800

    Update base.py

--- LLaVA-1.5-7B/base.py
+++ LLaVA-1.5-7B/base.py
@@ -1,29 +1,25 @@
 import os
 import json
 import re
-import random
 import torch
 from PIL import Image
 from datasets import Dataset
-from transformers import Qwen3VLForConditionalGeneration, AutoProcessor
+from transformers import LlavaForConditionalGeneration
 from trl import GRPOConfig, GRPOTrainer
-
-from utils import *
 
 
 model_path = "..."
 output_dir = "..."
-data_dir = "/datasets/..."
+data_dir = "..."
 
-
-model = Qwen3VLForConditionalGeneration.from_pretrained(
-    model_path, dtype=torch.bfloat16
-)
-
-processor = AutoProcessor.from_pretrained(
+model = LlavaForConditionalGeneration.from_pretrained(
     model_path,
-    use_fast=False
+    dtype=torch.bfloat16,
+    device_map="auto"
 )
+
+
+from utils import *
 
 
 data = load_train(data_dir)
@@ -33,9 +29,9 @@ training_args = GRPOConfig(
     output_dir=output_dir,
     num_generations=4,
     num_train_epochs=1,
-    per_device_train_batch_size=2,
-    gradient_accumulation_steps=4,
-    learning_rate=7e-6,
+    per_device_train_batch_size=4,
+    gradient_accumulation_steps=2,
+    learning_rate=5e-6,
     max_prompt_length=None,
     max_completion_length=512,
     temperature=1.0,
@@ -50,7 +46,6 @@ trainer = GRPOTrainer(
     model=model,
     args=training_args,
     train_dataset=data,
-    processing_class=processor,
     reward_funcs=[sqa_reward_func, reason_reward_func]
 )
 
